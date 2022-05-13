6f850cfb37774f7fc7225f0e7055d537c4c83f62
Romero <romeromendonca22@gmail.com>
2022-05-13 13:59:12 -0300

    Criando funções para gerar a base de dados sobre o banco de dados

--- lib/database/firebase_dao.dart
+++ lib/database/firebase_dao.dart
@@ -5,6 +5,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:modelo_app/components/diretorio.dart';
 
+// import '../data/folders_list.dart';
+
 class FirebaseDao extends ChangeNotifier {
   final Reference _firebaseStorageRef = FirebaseStorage.instance.ref();
 
@@ -38,7 +40,7 @@ class FirebaseDao extends ChangeNotifier {
     for (String element in listFullPaths) {
       await _firebaseStorageRef.child(element).delete();
     }
-    print('Apagão Finalizado');
+    debugPrint('Apagão Finalizado');
   }
 
   Future<int> incrementNumero(String child, int numero) async {
@@ -146,33 +148,41 @@ class FirebaseDao extends ChangeNotifier {
 
   // 1: Busca as pastas
   Future<void> updateVogalEPart1() async {
+    debugPrint('Iniciando verredura de pastas');
     ListResult data = await _firebaseStorageRef.child('folders').listAll();
     List<Reference> subFolders = data.prefixes;
     List foldersAdress = [];
     for (Reference subFolder in subFolders) {
       foldersAdress.add(subFolder.fullPath);
     }
-
-    List<String> _selectedFiles = await _updateVogalEPart2(foldersAdress);
-    print('--------------------- VOGAIS SELECIONADAS --------------------- ');
+    debugPrint('Varredura de pastas concluida!');
+    List _selectedFiles = await _updateVogalEPart2(foldersAdress);
+    debugPrint(
+        '--------------------- PASTAS SELECIONADAS --------------------- ');
+    debugPrint(_selectedFiles.length.toString());
+    debugPrint(_selectedFiles.toString());
     //await _updateVogalEfinal(_selectedFiles);
   }
 
   // 2: Busca todos os arquivos
-  Future<List<String>> _updateVogalEPart2(List foldersAdress) async {
+  Future<List> _updateVogalEPart2(List foldersAdress) async {
     List listFilesAdress = [];
     for (String fileAdress in foldersAdress) {
       ListResult files = await _firebaseStorageRef.child(fileAdress).listAll();
       List<Reference> filesReference = files.items;
       List filesAdressInFolder = [];
       for (Reference file in filesReference) {
-        filesAdressInFolder.add(file.fullPath);
+        if (file.name.contains('-')) {
+          filesAdressInFolder.add(file.name);
+        }
       }
-      listFilesAdress.add(filesAdressInFolder);
+      if (filesAdressInFolder.length != 0) {
+        listFilesAdress.add(filesAdressInFolder);
+      }
     }
 
-    List<String> _selectedFiles = _updateVogalEPart3(listFilesAdress);
-    return _selectedFiles;
+    // List<String> _selectedFiles = _updateVogalEPart3(listFilesAdress);
+    return listFilesAdress;
   }
 
   // 3: Seleciona apenas o arquivo txt da vogal 'é' em todas as pastas
@@ -181,28 +191,28 @@ class FirebaseDao extends ChangeNotifier {
     for (List list in listFilesAdress) {
       if (list.length > 2) {
         selectedFilesList.add(list[2]);
-        print(list);
+        debugPrint(list.toString());
       }
     }
     return selectedFilesList;
   }
 
   // 4: Altera o conteúdo do arquivo
-  // Future<void> _updateVogalEfinal(List listFilesAdress) async {
-  //   print("UPDATING...");
-  //   String pathTranscricaoWav =
-  //       await Diretorio('/GravacaoApp').getNomeDoArquivo('vogalE.txt');
-  //   io.File transcricao = io.File(pathTranscricaoWav);
-  //   transcricao.writeAsString('é é é');
-  //   for (String fileAdress in listFilesAdress) {
-  //     await _firebaseStorageRef
-  //         .child(fileAdress)
-  //         .putFile(File(pathTranscricaoWav));
-  //   }
-  //   print('FIM!');
-  // }
+  Future<void> _updateVogalEfinal(List listFilesAdress) async {
+    debugPrint("UPDATING...");
+    String pathTranscricaoWav =
+        await Diretorio('/GravacaoApp').getNomeDoArquivo('vogalE.txt');
+    io.File transcricao = io.File(pathTranscricaoWav);
+    transcricao.writeAsString('é é é');
+    for (String fileAdress in listFilesAdress) {
+      await _firebaseStorageRef
+          .child(fileAdress)
+          .putFile(File(pathTranscricaoWav));
+    }
+    debugPrint('FIM!');
+  }
 
-  // Gerar dados do banco do firebase
+  // Buscar Pastas do Storage
 
   Future<void> getFolders() async {
     ListResult data = await _firebaseStorageRef.child('folders').listAll();
@@ -220,10 +230,69 @@ class FirebaseDao extends ChangeNotifier {
     for (String fileAdress in foldersAdress) {
       ListResult files = await _firebaseStorageRef.child(fileAdress).listAll();
       if (files.items.length < 14) {
-        print(
+        debugPrint(
             'O usuário da pasta $fileAdress capturour apenas ${files.items.length / 2}');
+      } else {
+        listFilesAdress.add(fileAdress);
       }
     }
-    print(listFilesAdress);
+    debugPrint(listFilesAdress.toString());
+  }
+
+  // Gerar dataset com informações dos locutores
+
+  Future<void> getIdLocutores() async {
+    // Busca todos locutores do Firebase
+    debugPrint('Iniciando...');
+    DatabaseEvent event =
+        await _realtimeDatabaseRef.child('voluntarios').once();
+    Map voluntarios = event.snapshot.value as Map;
+    debugPrint('Simbora');
+    await _generateDatasetMap(voluntarios);
+  }
+
+  Future<void> _generateDatasetMap(Map voluntarios) async {
+    debugPrint('Gerando Listas!');
+    List locutoresNumPasta = [];
+    List locutoresEmail = [];
+    List locutoresNome = [];
+    List locutoresIdade = [];
+    List locutoresSexo = [];
+    List locutoresFluente = [];
+
+    // Gera um Map semalhante a um Pandas Dataframe
+    // voluntarios.forEach((key, value) {
+    //   if (foldersList.contains(value['numPasta'])) {
+    //     locutoresNumPasta.add('"${value['numPasta']}"');
+    //     locutoresEmail.add('"${value['email']}"');
+    //     locutoresNome.add('"${value['nome']}"');
+    //     locutoresIdade.add(value['idade']);
+    //     locutoresSexo.add('"${value['sexo']}"');
+    //     locutoresFluente.add(value['fluente']);
+    //   }
+    // });
+    debugPrint('Lista Geradas!');
+    Map dataset = {
+      'id_pasta': locutoresNumPasta,
+      'email': locutoresEmail,
+      'nome': locutoresNome,
+      'idade': locutoresIdade,
+      'sexo': locutoresSexo,
+      'fluente': locutoresFluente
+    };
+    debugPrint('Dataset gerado!');
+    await _createAndUploadDataset(dataset);
+    debugPrint('Sucesso!');
+  }
+
+  Future<void> _createAndUploadDataset(Map dataset) async {
+    String generateDir =
+        await Diretorio('/GravacaoApp').getNomeDoArquivo('dataset_info.json');
+    io.File generateFile = io.File(generateDir);
+    await generateFile.writeAsString(dataset.toString());
+
+    await _firebaseStorageRef
+        .child('dataset_info.json')
+        .putFile(File(generateDir));
   }
 }
